0f09daa0b31125ae76d89014e88fb470d210be4a
Jin Chen <ychen4000@hotmail.com>
2015-08-22 21:31:06 +1200

    Demo UI
    to do, Databasehelper and fragmentsController

---
+++ MyLifeLog/app/src/main/java/MyUtility/MyFragmentsController.java
@@ -0,0 +1,19 @@
+package MyUtility;
+
+/**
+ * Created by Administrator on 2015/8/22.
+ */
+public class MyFragmentsController {
+
+
+
+    public boolean SwichToStartPage ( )
+    {
+        boolean Switch = false;
+
+
+        return Switch;
+    }
+
+
+}
